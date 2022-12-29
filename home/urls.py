199dd3ff7282a3829ae0158cc74922f031679847
rayyandeveloper <rayyan.dev@mail.ru>
2022-12-29 23:49:50 +0500

    xxx

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home-page'),
+    path('delete-people/', delete, name='delete-people'),
     path('search/', search),
     path('lits/', lits, name="lits-page"),
     path('peoples/', peoples, name="peoples-page"),
--- home/views.py
+++ home/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 def home(request):
     context = {'page_name' : 'home'}
     return render(request, 'home-page.html', context=context)
-
-
-
 
 
 def lits(request):
@@ -17,12 +14,38 @@ def lits(request):
 
 
 def peoples(request):
-
-
     peoples = People.objects.all()
     courses = Course.objects.all()
     subjects = Subject.objects.all()
 
+    q = request.GET.get('q')
+
+    if request.method == 'POST':
+        if request.POST.get('type'):
+            id = request.POST.get('id')
+            full_name = request.POST.get('full-name')
+            birthday = request.POST.get('birthday')
+            phone = request.POST.get('phone')
+            
+            people = People.objects.get(pk=id)
+            
+            people.full_name = full_name
+            people.birthday = birthday
+            people.phone = phone
+            people.save()
+        else:
+            data = request.POST
+            full_name = data['full-name']
+            birthday = data['birthday']
+            phone = data['phone']
+
+            new_people = People.objects.create(full_name=full_name, birthday=birthday, phone=phone)
+            
+
+    if q:
+        peoples = People.objects.filter(full_name__icontains=q)
+
+    
     context = {'page_name' : 'peoples', 'peoples' : peoples, 'subjects' : subjects, 'courses' : courses,}
     return render(request, 'peoples.html', context=context)
 
@@ -53,3 +76,10 @@ def search(request):
     if request.method == 'POST':
         pass
     return render(request, 'index.html')
+
+
+def delete(request):
+    id = request.GET.get('id')
+    people = People.objects.get(pk=id).delete()
+    
+    return redirect(request.META.get('HTTP_REFERER'))
